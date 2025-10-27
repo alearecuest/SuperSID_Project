727848f4bce32642c74da7154db08bc5f967c382
Alejandro Arévalo <126989105+alearecuest@users.noreply.github.com>
2025-10-27 17:53:49 -0300

    Add basic Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from '@chakra-ui/react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link href="/">Home</Link>
+        </nav>
+    );
+};
+
+export default Navbar;
