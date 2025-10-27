9daca9fd2e725c1f90d60d2f58db10a63c90a44f
Alejandro <10653@holbertonstudents.com>
2025-10-27 17:44:11 -0300

    Init

--- test_config.py
+++
@@ -1,5 +0,0 @@
-from supersid.utils.config_loader import load_config
-
-cfg = load_config("supersid/configs/montevideo.toml",
-                  "supersid/configs/transmitters.toml")
-print(cfg["channels"])
